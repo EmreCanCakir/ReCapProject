2f6eab7d5ea07e8bf5d1eb6e373bdb0112cf4f91
EmreCanCakir <emreecann28@gmail.com>
2022-03-07 20:19:50 +0300

    Some Changes of day 7

--- Business/Abstracts/ICarService.cs
+++ Business/Abstracts/ICarService.cs
@@ -9,6 +9,11 @@ namespace Business.Abstracts
 {
     public interface ICarService
     {
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
         List<Car> GetAll();
+        Car GetById(int id);
+
     }
 }
